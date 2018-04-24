ed18d5f670bc64842c0bf0cf07a0759d3001dd60
Paul Cheno <pwchenoweth@gmail.com>
2018-04-24 17:47:43 -0500

    puting server together

---
+++ server.js
@@ -0,0 +1,14 @@
+const ex = require("express");
+const bp = require("body-parser");
+const p  = require("path");
+
+const app = ex();
+const PORT = process.env.PORT || 3000;
+
+app.use(bp.urlencoded({ extended: true}));
+app.use(bp.json());
+
+app.use(ex.static(p.join(__dirname, "/app/public")));
+
+require("./app/routing/apiRoutes.js")(app);
+require("")
